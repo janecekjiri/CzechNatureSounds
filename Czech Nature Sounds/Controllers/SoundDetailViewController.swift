3110a24c9c163f44849ae46a5ccf957a4110470a
jirijanecek <jiri.janecek@yahoo.com>
2023-07-04 08:53:53 +0200

    2.0.0 Added Detail VC

---
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b55e76cd819af18e42af8a4532767c76ef4a1902a07bc83df04c1a561da3086a
+size 987
--- Czech Nature Sounds/Controllers/SoundsListViewController.swift
+++ Czech Nature Sounds/Controllers/SoundsListViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:131a065007b32506374fa6ea85d7be03a9dcd5a93fe4ecc29e82f5fcf97e59e4
-size 2426
+oid sha256:e2095fe9985dfcf3ff19c48ecb08575f35e8921bdcedff65ae58656f1d54ae10
+size 2710
