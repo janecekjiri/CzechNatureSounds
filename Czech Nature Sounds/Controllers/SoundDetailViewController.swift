b77a40a50613a7ed1b9340df6ce5fa0c0db48d1b
jirijanecek <jiri.janecek@yahoo.com>
2023-07-05 10:31:24 +0200

    2.0.0 Fixed play button's title not changing

--- Czech Nature Sounds/Controllers/SoundDetailViewController.swift
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e177c36dbdc123365e7f358e955528f5ae45a3cc6398a163a6bcbabcf0aa79b
-size 7643
+oid sha256:793e773e00bb498177062c233c1988554ec4fd935b08c23e011b867ea6ad5c61
+size 7699
