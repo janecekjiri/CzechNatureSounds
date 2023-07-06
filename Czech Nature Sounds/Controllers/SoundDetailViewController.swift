b43c3fffbf80eceb0b14aef7f3ad251ec2933978
jirijanecek <jiri.janecek@yahoo.com>
2023-07-06 14:04:53 +0200

    2.0.0 Music stops playing when leaving detail

--- Czech Nature Sounds/Controllers/SoundDetailViewController.swift
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1b00847c03cba2ac50c92ab83b31251b157bb089a5833118c943ca93a25f1b
-size 9184
+oid sha256:3195cf2b97116ebacbff71a32d588e66490a8d8e55eb22ed853548b081ceb708
+size 9358
