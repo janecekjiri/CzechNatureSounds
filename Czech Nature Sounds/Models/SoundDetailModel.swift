2e0e7c3eab6b5a5cbda4782d7c729f06bae9152b
jirijanecek <jiri.janecek@yahoo.com>
2023-07-08 15:01:05 +0200

    2.0.0 When playing the sound track again, it plays from the very beginning

--- Czech Nature Sounds/Models/SoundDetailModel.swift
+++ Czech Nature Sounds/Models/SoundDetailModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aeddfff328062abab9531f294c8e5fb8c1084307e17c905fdb6ae5ad2503fd0
-size 4297
+oid sha256:43b691e6a9b2bbf5f8a78394dc35029f1269606ed0995d92cb28687c9839867b
+size 4346
