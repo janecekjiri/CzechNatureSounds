567e4cad552c9d5e83eeef17d8ac147ca0a227d2
jirijanecek <jiri.janecek@yahoo.com>
2023-07-08 14:28:28 +0200

    2.0.0 Fixed strong references

--- Czech Nature Sounds/Controllers/SoundDetailViewController.swift
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:564a5906a394e6acc6efec0820f28ea16704a54e74ed218aa112c23d4167cec8
-size 8621
+oid sha256:65b60a28d07b2daa7ca8b0983d560c3611e187cd57da5ac1d266e594be891b41
+size 8661
