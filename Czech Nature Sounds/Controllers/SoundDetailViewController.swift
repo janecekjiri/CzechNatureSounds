5286a6e8d9523e436940792714de48297d869f81
jirijanecek <jiri.janecek@yahoo.com>
2023-07-11 19:56:38 +0200

    2.0.0 Moved views from detail's VC to its own class

--- Czech Nature Sounds/Controllers/SoundDetailViewController.swift
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c9d377d47954919d0cfaa27d4d6539858e5d0d775d2012a97c8efb9101b4a1b
-size 10253
+oid sha256:337f238ff21f3aaadce08e4d882b615db49b4cff70dd033f9c479f3593c14e5c
+size 5555
---
+++ Czech Nature Sounds/Views/SoundDetailView.swift
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:295cc178832170f2945e6bd2173e7103fd49ba7d6b32170b1f124080a169a74a
+size 6771
