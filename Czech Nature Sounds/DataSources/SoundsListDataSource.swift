92b7e402c39aa8c7352bee1b19bf6689ad7c247b
jirijanecek <jiri.janecek@yahoo.com>
2023-07-11 08:28:01 +0200

    2.0.0 Fixed access control for SoundsListDataSource

--- Czech Nature Sounds/DataSources/SoundsListDataSource.swift
+++ Czech Nature Sounds/DataSources/SoundsListDataSource.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b05524af7fcd69a035b8348b680b6bf0b836ebbe39e669cd9f33b13af10e3a8
-size 973
+oid sha256:d139952c2191828b195c2752bcc9681266880cb4bfacaff88c8f7c11c394336c
+size 968
