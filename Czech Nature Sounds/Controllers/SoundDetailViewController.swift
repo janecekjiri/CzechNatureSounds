12405c87c35691d7b02d3b207fe098e8f3fe39e3
jirijanecek <jiri.janecek@yahoo.com>
2023-07-20 17:49:05 +0200

    2.0.0 Added ability to control player from lock screen

--- Czech Nature Sounds/Controllers/SoundDetailViewController.swift
+++ Czech Nature Sounds/Controllers/SoundDetailViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4319e4470cd62a17c7625e5596f3d3dbbcd4946f5358dce9fef9bc8091334bb6
-size 5953
+oid sha256:e6d0791628a197dbcf78fbe073985cf584249fdd4b7125de0c1636b2bcc37bea
+size 6774
