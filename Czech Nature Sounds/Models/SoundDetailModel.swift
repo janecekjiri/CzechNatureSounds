512df1124688714fa87bf5c94cf3a82f995340f5
jirijanecek <jiri.janecek@yahoo.com>
2023-07-21 16:52:29 +0200

    2.0.0 Fixed audio not resuming after interruption ending

--- Czech Nature Sounds/Models/SoundDetailModel.swift
+++ Czech Nature Sounds/Models/SoundDetailModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21f311ba3ad7db417df329c93a91e52900b7d7d56c9c561485ae7d8a778a9465
-size 6837
+oid sha256:7ba08af09699fea68089a45719cf5e47a49aa821c5a22d421bf36967e7ba7526
+size 6808
