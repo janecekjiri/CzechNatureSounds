08e506a5aac8e5b940c40a8e7df0b0a131edd327
jirijanecek <jiri.janecek@yahoo.com>
2023-07-22 14:34:37 +0200

    2.0.0 Added tests

--- Czech Nature Sounds/Models/SoundDetailModel.swift
+++ Czech Nature Sounds/Models/SoundDetailModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ba08af09699fea68089a45719cf5e47a49aa821c5a22d421bf36967e7ba7526
-size 6808
+oid sha256:65959380874acc65dc83a178fa2cc6b605bae1f8bb06dbf8cc331d0d4314302d
+size 7110
---
+++ CzechNatureSoundsTests/SoundDetailModelTests.swift
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:574dbd05fe7ae64a77946d895bfbea5f77a645a7d683e23a4627b0ac037dae30
+size 1980
